0910c41b7eeaa18572c9d98114401c9aed7d4596
Dominik Kedziak <dominik.kedziak@allegro.pl>
2022-01-11 10:37:58 +0100

    plugin

--- jupyterlab_gcloud_auth/__init__.py
+++ jupyterlab_gcloud_auth/__init__.py
@@ -27,15 +27,8 @@ def _jupyter_server_extension_points():
 
 
 def _load_jupyter_server_extension(server_app):
-    """Registers the API handler to receive HTTP requests from the frontend extension.
-
-    Parameters
-    ----------
-    server_app: jupyterlab.labapp.LabApp
-        JupyterLab application instance
-    """
     setup_handlers(server_app.web_app)
-    server_app.log.info("Registered HelloWorld extension at URL path /gcloud-auth")
+    server_app.log.info("Registered jupyterlab-gcloud-auth extension at URL path /gcloud-auth")
 
 # For backward compatibility with notebook server - useful for Binder/JupyterHub
 load_jupyter_server_extension = _load_jupyter_server_extension
